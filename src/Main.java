80f48f9c3d7a3f4250077d5c6267c5de97e5f73e
Kübra Aykan <aykan.kubra99@gmail.com>
2023-04-18 22:36:36 +0300

    added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int year;
+
+        Scanner input=new Scanner (System.in);
+        System.out.print("Lütfen yılı giriniz:");
+        year=input.nextInt();
+
+        boolean artikYilMi=false;
+
+        if(year%4==0 ){
+            if (year%100==0 ) {
+                if (year%400==0){
+                    artikYilMi=true;
+                }
+            }else {
+                artikYilMi=true;
+            }
+        }
+        if (artikYilMi){
+            System.out.println(year+ " Artık bir yıldır.");
+        }else {
+            System.out.println(year+ " Artık bir yıl değildir.");
+        }
+        }
     }
-}
